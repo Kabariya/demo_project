34bd7a52c06a267327b4b881c23c8b9023219704
kuldeep <17itkuldeepvagadiya@gmail.com>
2021-07-21 15:14:48 +0530

    # change app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Test',
+      title: 'Flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
